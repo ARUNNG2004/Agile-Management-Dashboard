f34588e52a58ea8c0d0c356981aa7f17fe443959
N G ARUN <92279619+ARUNNG2004@users.noreply.github.com>
2025-04-12 13:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from app import create_app
+from vercel_wsgi import handle_request
+from app import create_app  # Ensure this imports the create_app function
 
 application = create_app()
 
-if __name__ == '__main__':
+def handler(request, context):
+    return handle_request(application, request, context)
 
+if __name__ == "__main__":
     application.run(debug=True)
-
-
